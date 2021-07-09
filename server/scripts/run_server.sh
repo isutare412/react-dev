02fa54faa358cc8c8c980534a7eb53f5ada88eff
Redshore <isutare412@gmail.com>
2021-07-10 01:21:37 +0900

    Containerize front-end and back-end server

--- server/scripts/run_server.sh
+++ server/scripts/run_server.sh
@@ -1,4 +1,4 @@
-export PORT=${PORT:-"4191"}
+export PORT=${PORT:-"4182"}
 export MODE=${MODE:-"development"}
 export MONGO_URL=${MONGO_URL:-"localhost:26521"}
 export MONGO_USERNAME=${MONGO_USERNAME:-"root"}
