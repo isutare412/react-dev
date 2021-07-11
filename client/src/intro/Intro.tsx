beafcdb0dc3edb39ccefe98fd276f18219f549ae
Redshore <isutare412@gmail.com>
2021-07-11 14:27:28 +0900

    Move product list to intoduction

--- client/src/intro/Intro.tsx
+++ client/src/intro/Intro.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import EmployeeList from "./EmployeeList";
+import ProductList from "./ProductList";
 
 export default function Intro(): JSX.Element {
   return (
@@ -7,8 +8,12 @@ export default function Intro(): JSX.Element {
       <h1 className="text-3xl font-extralight text-red-500 my-4">
         Introduction
       </h1>
+
       <h2 className="text-lg text-red-400">Employees</h2>
       <EmployeeList />
+
+      <h2 className="text-lg text-red-400 mt-5">Products</h2>
+      <ProductList />
     </div>
   );
 }
